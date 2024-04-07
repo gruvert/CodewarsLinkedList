27354e9184927f66f91110da28a89804e69ea7f6
gruvert <147984995+gruvert@users.noreply.github.com>
2024-04-07 17:52:36 +0300

    created move_node function to move data from sourse to dest

--- Linked Lists - Move Node/solution.py
+++ Linked Lists - Move Node/solution.py
@@ -1,1 +1,22 @@
-
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+    
+def move_node(source, dest):
+    if not source:
+        raise Exception
+    if not dest:
+        dest = Node(source.data)
+    else:
+        dest = push(dest, source.data)
+    if source.next:
+        source = source.next
+    else:
+        source = None
+    return Context(source, dest)
