5eb0a84f48fd27105b9c50e674dbd45187010362
gruvert <147984995+gruvert@users.noreply.github.com>
2024-04-07 18:32:39 +0300

    created sorted_insert function to insert in right position data in sorted list

--- Linked Lists - Sorted Insert/solution.py
+++ Linked Lists - Sorted Insert/solution.py
@@ -1,1 +1,16 @@
-
+""" For your information:
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+"""
+def sorted_insert(head, data):
+    result = Node(data)
+    if head is None or head.data >= data:
+        result.next = head
+        return result
+    current = head
+    while current.next and current.next.data < data:
+        current = current.next
+    result.next = current.next
+    current.next = result
